e0e4bb75cc01264d7bdaa50f7826c4ce63d427ee
mhay10 <maxwellhay1@gmail.com>
2021-04-11 15:39:12 -0600

    Add windows support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # vim-setup
 This is a github repo that allows you to setup vim according to my prefrences quickly and easily.\
 Just run `vim-setup.sh` as and everything will be done for you.\
-Note: If you run `vim-setup.sh` as root, it will setup vim for the root user.
+Note: If you run `vim-setup.sh` as root, it will setup vim for the root user.\
+
+For Windows run `vim-setup.ps1` in a powershell prompt.\
 
 **Note:** This script will overwrite all current settings in vim. You have been warned.
---
+++ vim-setup.ps1
@@ -0,0 +1,32 @@
+# Make sure you want to continue
+Write-Host "Warning: This will REPLACE the _vimrc file in home dir"
+$check = Read-Host -Prompt "Are you sure you want to continue? (y/n)"
+
+# Make sure you enter the correct response
+$values = @("y", "Y", "n", "N")
+while (!$values.Contains($check)) {
+    $check = Read-Host -Prompt "Are you sure you want to continue? (y/n)"
+}
+
+# What to do when correct response entered
+if ( $check == "y" -or $check == "Y") {
+    # Check if vim-plug is installed. If not, install it
+    if (!Test-Path "$env:UserProfile\vimfiles\autoload") {
+        mkdir -Path "$env:UserProfile\vimfiles\autoload"
+
+        $pluginUrl = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        Invoke-WebRequest -Uri $pluginUrl -OutFile "$env:UserProfile\vimfiles\autoload\plug.vim"
+    }
+
+    # Make the vimrc file in $env:UserProfile
+    if (Test-Path "$env:UserProfile\_vimrc") {
+        Remove-Item -Recurse -Force -Path "$env:UserProfile\_vimrc"
+        Copy-Item ".vimrc" "$env:UserProfile\_vimrc"
+    } else {
+        Copy-ITem ".vimrc" "$env:UserProfile\_vimrc"
+    }
+
+    Write-Host "_vimrc file has been overwritten."
+} elseif ($check == "n" -or $check == "N") {
+    Write-Host "Okay, bye!"
+}
